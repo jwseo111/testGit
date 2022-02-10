aa2eb74502afb88255b90d2086b6c59b2ae8bf0d
JinwooSeo <wlsdn5209@gmail.com>
2022-02-10 16:23:20 +0900

    qefcczx

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
 		return "Spring is here!";
 	}
 
-	public static void main(String[] args) { //test444
+	public static void main(String[] args) { //test444333
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
